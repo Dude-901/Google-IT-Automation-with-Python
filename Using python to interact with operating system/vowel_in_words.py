296af4c2062bb2153f7dd50bb4081d66466ffad5
Tanmay Khandelwal <58903502+Dude-901@users.noreply.github.com>
2020-09-08 14:49:38 +0530

    find words with more than 3 vowels

--- Using python to interact with operating system/vowel_in_words.py
+++ Using python to interact with operating system/vowel_in_words.py
@@ -6,3 +6,15 @@ def multi_vowel_words(text):
 
 print(multi_vowel_words("Life is beautiful")) 
 # ['beautiful']
+
+print(multi_vowel_words("Obviously, the queen is courageous and gracious.")) 
+# ['Obviously', 'queen', 'courageous', 'gracious']
+
+print(multi_vowel_words("The rambunctious children had to sit quietly and await their delicious dinner.")) 
+# ['rambunctious', 'quietly', 'delicious']
+
+print(multi_vowel_words("The order of a data queue is First In First Out (FIFO)")) 
+# ['queue']
+
+print(multi_vowel_words("Hello world!")) 
+# []
